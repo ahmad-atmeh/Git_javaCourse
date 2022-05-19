3c9ac29f60f4d5fbcf4b706fa218031f5165c344
ahmad.atmeh50 <ahmad.atmeh50@gmail.com>
2022-05-19 20:44:30 +0300

    file find sum of tow number

---
+++ first/src/test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test {
+     public static void main(String[] args) {
+         Scanner inpyt = new Scanner(System.in);
+         int x = inpyt.nextInt();
+         int y = inpyt.nextInt();
+                
+           System.out.println(x + y);    
+    }
+    
+}
